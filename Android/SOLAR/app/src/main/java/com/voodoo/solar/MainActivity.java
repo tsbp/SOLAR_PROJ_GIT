1e83c408b2f43875004b5b44326335f3905ac416
tsbp <tsbp82@gmail.com>
2017-11-22 16:53:29 +0200

    setting activity added

--- Android/SOLAR/app/src/main/java/com/voodoo/solar/MainActivity.java
+++ Android/SOLAR/app/src/main/java/com/voodoo/solar/MainActivity.java
@@ -34,11 +34,6 @@ import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
 
 import static com.voodoo.solar.IPHelper.getBroadcastIP4AsBytes;
-import static java.lang.Math.PI;
-import static java.lang.Math.atan;
-import static java.lang.Math.cos;
-import static java.lang.Math.sin;
-
 
 public class MainActivity extends Activity implements OnReceiveListener  {
 
@@ -80,8 +75,8 @@ public class MainActivity extends Activity implements OnReceiveListener  {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        GLSurfaceView view = (GLSurfaceView) findViewById(R.id.w3D);
-//        view.setRenderer(new OpenGLRenderer());
+        GLSurfaceView view = (GLSurfaceView) findViewById(R.id.w3D);
+        view.setRenderer(new OpenGLRenderer());
 
 //        tCompass = (TextView) findViewById(R.id.tvCompass);
 //        tAccel   = (TextView) findViewById(R.id.tvAccel);
@@ -380,7 +375,7 @@ public class MainActivity extends Activity implements OnReceiveListener  {
 
         if(aIP != null && buf != null)
         {
-            buf[0] = (byte) ID_MASTER;
+            buf[0] = ID_MASTER;
             buf[1] = aCmd;
             buf[2] = (byte) dataLng;
 
@@ -470,11 +465,11 @@ public class MainActivity extends Activity implements OnReceiveListener  {
                         if((in[11] & (short)8) == 8)   terms += "1"; //btnIn4.setBackgroundColor(Color.RED);
                         else                           terms += "0"; //btnIn4.setBackgroundColor(Color.GREEN);
 
-                        int light = (((in[10] << 8) & 0xff00) | (int) (in[9] & 0xff));
+                        int light = (((in[10] << 8) & 0xff00) | in[9] & 0xff);
 
-                        ax = (short) (((in[4] << 8) & 0xff00) | (int) (in[3] & 0xff));
-                        ay = (short) (((in[6] << 8) & 0xff00) | (int) (in[5] & 0xff));
-                        az = (short) (((in[8] << 8) & 0xff00) | (int) (in[7] & 0xff));
+                        ax = (short) (((in[4] << 8) & 0xff00) | in[3] & 0xff);
+                        ay = (short) (((in[6] << 8) & 0xff00) | in[5] & 0xff);
+                        az = (short) (((in[8] << 8) & 0xff00) | in[7] & 0xff);
 
 
                         double tt = Math.toDegrees((double)az/10000);
@@ -491,6 +486,12 @@ public class MainActivity extends Activity implements OnReceiveListener  {
                                 clientData[i][4] = terms;
                                 lvBuid();
                                 notAsweredCntr[i] = 0;
+
+                                if(i == 0)
+                                {
+                                    pPitch = (double)ax/10000;
+                                    pRoll  = (double)ay/10000;
+                                }
                             }
                         }
 
@@ -516,124 +517,124 @@ public class MainActivity extends Activity implements OnReceiveListener  {
 
 }
 
-class OpenGLRenderer implements GLSurfaceView.Renderer {
-
-    private Cube mCube = new Cube();
-    private float mCubeRotation;
-
-    @Override
-    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
-        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
-
-        gl.glClearDepthf(1.0f);
-        gl.glEnable(GL10.GL_DEPTH_TEST);
-        gl.glDepthFunc(GL10.GL_LEQUAL);
-
-        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
-                GL10.GL_NICEST);
-
-    }
-
-    @Override
-    public void onDrawFrame(GL10 gl) {
-        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
-        gl.glLoadIdentity();
-
-        gl.glTranslatef(0.0f, 0.0f, -10.0f);
-        //gl.glRotatef(mCubeRotation, 1.0f, 1.0f, 1.0f);
-
-        gl.glRotatef(mCubeRotation, 0.0f, 0.0f, 1.0f);
-        gl.glRotatef((float)Math.toDegrees(MainActivity.pRoll), 1.0f, 0.0f, 0.0f);
-        gl.glRotatef((float)Math.toDegrees(MainActivity.pHead), 0.0f, 1.0f, 0.0f);
-
-        mCube.draw(gl);
-
-        gl.glLoadIdentity();
-
-        mCubeRotation = (-1) * (float) Math.toDegrees(MainActivity.pPitch);
-        //mCubeRotation -= 0.15f;
-    }
-
-    @Override
-    public void onSurfaceChanged(GL10 gl, int width, int height) {
-        gl.glViewport(0, 0, width, height);
-        gl.glMatrixMode(GL10.GL_PROJECTION);
-        gl.glLoadIdentity();
-        GLU.gluPerspective(gl, 20.0f, (float)width / (float)height, 0.1f, 100.0f);
-        gl.glViewport(0, 0, width, height);
-
-        gl.glMatrixMode(GL10.GL_MODELVIEW);
-        gl.glLoadIdentity();
-    }
-}
-
-class Cube {
-
-    private FloatBuffer mVertexBuffer;
-    private FloatBuffer mColorBuffer;
-    private ByteBuffer  mIndexBuffer;
-
-    private float vertices[] = {
-            -1.0f, -0.2f, -1.0f,
-            1.0f, -0.2f, -1.0f,
-            1.0f,  0.2f, -1.0f,
-            -1.0f, 0.2f, -1.0f,
-            -1.0f, -0.2f,  1.0f,
-            1.0f, -0.2f,  1.0f,
-            1.0f,  0.2f,  1.0f,
-            -1.0f,  0.2f,  1.0f
-    };
-    private float colors[] = {
-            0.0f,  1.0f,  0.0f,  1.0f,
-            0.0f,  1.0f,  0.0f,  1.0f,
-            1.0f,  0.5f,  0.0f,  1.0f,
-            1.0f,  0.5f,  0.0f,  1.0f,
-            1.0f,  0.0f,  0.0f,  1.0f,
-            1.0f,  0.0f,  0.0f,  1.0f,
-            0.0f,  0.0f,  1.0f,  1.0f,
-            1.0f,  0.0f,  1.0f,  1.0f
-    };
-
-    private byte indices[] = {
-            0, 4, 5, 0, 5, 1,
-            1, 5, 6, 1, 6, 2,
-            2, 6, 7, 2, 7, 3,
-            3, 7, 4, 3, 4, 0,
-            4, 7, 6, 4, 6, 5,
-            3, 0, 1, 3, 1, 2
-    };
-
-    public Cube() {
-        ByteBuffer byteBuf = ByteBuffer.allocateDirect(vertices.length * 4);
-        byteBuf.order(ByteOrder.nativeOrder());
-        mVertexBuffer = byteBuf.asFloatBuffer();
-        mVertexBuffer.put(vertices);
-        mVertexBuffer.position(0);
-
-        byteBuf = ByteBuffer.allocateDirect(colors.length * 4);
-        byteBuf.order(ByteOrder.nativeOrder());
-        mColorBuffer = byteBuf.asFloatBuffer();
-        mColorBuffer.put(colors);
-        mColorBuffer.position(0);
-
-        mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
-        mIndexBuffer.put(indices);
-        mIndexBuffer.position(0);
-    }
-
-    public void draw(GL10 gl) {
-        gl.glFrontFace(GL10.GL_CW);
-
-        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
-        gl.glColorPointer(4, GL10.GL_FLOAT, 0, mColorBuffer);
-
-        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
-
-        gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE,
-                mIndexBuffer);
-
-        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
-        gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
-    }
-}
+//class OpenGLRenderer implements GLSurfaceView.Renderer {
+//
+//    private Cube mCube = new Cube();
+//    private float mCubeRotation;
+//
+//    @Override
+//    public void onSurfaceCreated(GL10 gl, EGLConfig config) {
+//        gl.glClearColor(0.0f, 0.0f, 0.0f, 0.5f);
+//
+//        gl.glClearDepthf(1.0f);
+//        gl.glEnable(GL10.GL_DEPTH_TEST);
+//        gl.glDepthFunc(GL10.GL_LEQUAL);
+//
+//        gl.glHint(GL10.GL_PERSPECTIVE_CORRECTION_HINT,
+//                GL10.GL_NICEST);
+//
+//    }
+//
+//    @Override
+//    public void onDrawFrame(GL10 gl) {
+//        gl.glClear(GL10.GL_COLOR_BUFFER_BIT | GL10.GL_DEPTH_BUFFER_BIT);
+//        gl.glLoadIdentity();
+//
+//        gl.glTranslatef(0.0f, 0.0f, -10.0f);
+//        //gl.glRotatef(mCubeRotation, 1.0f, 1.0f, 1.0f);
+//
+//        gl.glRotatef(mCubeRotation, 0.0f, 0.0f, 1.0f);
+//        gl.glRotatef((float)Math.toDegrees(MainActivity.pRoll), 1.0f, 0.0f, 0.0f);
+//        gl.glRotatef((float)Math.toDegrees(MainActivity.pHead), 0.0f, 1.0f, 0.0f);
+//
+//        mCube.draw(gl);
+//
+//        gl.glLoadIdentity();
+//
+//        mCubeRotation = (-1) * (float) Math.toDegrees(MainActivity.pPitch);
+//        //mCubeRotation -= 0.15f;
+//    }
+//
+//    @Override
+//    public void onSurfaceChanged(GL10 gl, int width, int height) {
+//        gl.glViewport(0, 0, width, height);
+//        gl.glMatrixMode(GL10.GL_PROJECTION);
+//        gl.glLoadIdentity();
+//        GLU.gluPerspective(gl, 20.0f, (float)width / (float)height, 0.1f, 100.0f);
+//        gl.glViewport(0, 0, width, height);
+//
+//        gl.glMatrixMode(GL10.GL_MODELVIEW);
+//        gl.glLoadIdentity();
+//    }
+//}
+//
+//class Cube {
+//
+//    private FloatBuffer mVertexBuffer;
+//    private FloatBuffer mColorBuffer;
+//    private ByteBuffer  mIndexBuffer;
+//
+//    private float vertices[] = {
+//            -1.0f, -0.2f, -1.0f,
+//            1.0f, -0.2f, -1.0f,
+//            1.0f,  0.2f, -1.0f,
+//            -1.0f, 0.2f, -1.0f,
+//            -1.0f, -0.2f,  1.0f,
+//            1.0f, -0.2f,  1.0f,
+//            1.0f,  0.2f,  1.0f,
+//            -1.0f,  0.2f,  1.0f
+//    };
+//    private float colors[] = {
+//            0.0f,  1.0f,  0.0f,  1.0f,
+//            0.0f,  1.0f,  0.0f,  1.0f,
+//            1.0f,  0.5f,  0.0f,  1.0f,
+//            1.0f,  0.5f,  0.0f,  1.0f,
+//            1.0f,  0.0f,  0.0f,  1.0f,
+//            1.0f,  0.0f,  0.0f,  1.0f,
+//            0.0f,  0.0f,  1.0f,  1.0f,
+//            1.0f,  0.0f,  1.0f,  1.0f
+//    };
+//
+//    private byte indices[] = {
+//            0, 4, 5, 0, 5, 1,
+//            1, 5, 6, 1, 6, 2,
+//            2, 6, 7, 2, 7, 3,
+//            3, 7, 4, 3, 4, 0,
+//            4, 7, 6, 4, 6, 5,
+//            3, 0, 1, 3, 1, 2
+//    };
+//
+//    public Cube() {
+//        ByteBuffer byteBuf = ByteBuffer.allocateDirect(vertices.length * 4);
+//        byteBuf.order(ByteOrder.nativeOrder());
+//        mVertexBuffer = byteBuf.asFloatBuffer();
+//        mVertexBuffer.put(vertices);
+//        mVertexBuffer.position(0);
+//
+//        byteBuf = ByteBuffer.allocateDirect(colors.length * 4);
+//        byteBuf.order(ByteOrder.nativeOrder());
+//        mColorBuffer = byteBuf.asFloatBuffer();
+//        mColorBuffer.put(colors);
+//        mColorBuffer.position(0);
+//
+//        mIndexBuffer = ByteBuffer.allocateDirect(indices.length);
+//        mIndexBuffer.put(indices);
+//        mIndexBuffer.position(0);
+//    }
+//
+//    public void draw(GL10 gl) {
+//        gl.glFrontFace(GL10.GL_CW);
+//
+//        gl.glVertexPointer(3, GL10.GL_FLOAT, 0, mVertexBuffer);
+//        gl.glColorPointer(4, GL10.GL_FLOAT, 0, mColorBuffer);
+//
+//        gl.glEnableClientState(GL10.GL_VERTEX_ARRAY);
+//        gl.glEnableClientState(GL10.GL_COLOR_ARRAY);
+//
+//        gl.glDrawElements(GL10.GL_TRIANGLES, 36, GL10.GL_UNSIGNED_BYTE,
+//                mIndexBuffer);
+//
+//        gl.glDisableClientState(GL10.GL_VERTEX_ARRAY);
+//        gl.glDisableClientState(GL10.GL_COLOR_ARRAY);
+//    }
+//}
