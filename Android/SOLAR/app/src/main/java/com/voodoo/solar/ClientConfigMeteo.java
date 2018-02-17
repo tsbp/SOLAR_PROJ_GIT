ef6fcfd020ed8e55376ab5f42674f267e40d479d
tsbp <tsbp82@gmail.com>
2018-02-17 12:49:21 +0200

    2345

---
+++ Android/SOLAR/app/src/main/java/com/voodoo/solar/ClientConfigMeteo.java
@@ -0,0 +1,348 @@
+package com.voodoo.solar;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.opengl.GLSurfaceView;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import java.net.InetAddress;
+
+import static com.voodoo.solar.MainActivity.BROADCAST_ACTION;
+
+public class ClientConfigMeteo extends Activity {
+
+    TextView tvIp, tvWind, tvTime, tvAzim, tvElev, tvWindS, tvlight;
+
+    public final static String BC_CFG_DATA = "broadcast CFG data";
+
+    public static InetAddress ip;
+    BroadcastReceiver br, br_cfg;
+
+    public static byte data[], cfgData[];
+    double azimuth, elevation;
+
+
+    public final static byte STOPPED   = (byte)0x0;
+    public final static byte TRACKING  = (byte)0x1;
+    public final static byte ALARM     = (byte)0x2;
+    public final static byte MANUAL_ALARM     = (byte)0x3;
+    byte meteoState = 0;
+
+    Button bServ, bAlarm;
+
+
+    com.voodoo.solar.imgPosition imgSun;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_meteo);
+
+        tvWind = (TextView) findViewById(R.id.tvWind);
+        tvTime = (TextView) findViewById(R.id.tvMeteoTime);
+        tvAzim = (TextView) findViewById(R.id.tvMeteoAzimuth);
+        tvElev = (TextView) findViewById(R.id.tvMeteoElevation);
+        tvWindS = (TextView) findViewById(R.id.tvMeteoWind);
+        tvlight = (TextView) findViewById(R.id.tvMeteoLight);
+
+        tvIp = (TextView) findViewById(R.id.tvIPMeteo);
+        tvIp.setText("" + ip.getHostAddress());
+
+        imgSun = (com.voodoo.solar.imgPosition) findViewById(R.id.imgPos);
+
+        //================================================
+        bServ = (Button) findViewById(R.id.btnMeteoServ);
+        bServ.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                byte buf[] = new byte[1];
+                if(meteoState == TRACKING) buf[0] = STOPPED;
+                else buf[0] = TRACKING;
+                UDPCommands.sendCmd(UDPCommands.CMD_SERVICE, buf, ip);
+            }
+        });
+
+        //================================================
+        bAlarm = (Button) findViewById(R.id.btnMeteoAlarm);
+        bAlarm.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                byte buf[] = new byte[1];
+                if(meteoState != MANUAL_ALARM) buf[0] = MANUAL_ALARM;
+                else buf[0] = STOPPED;
+                UDPCommands.sendCmd(UDPCommands.CMD_SERVICE, buf, ip);
+            }
+        });
+
+        //================================================
+        br = new BroadcastReceiver() {
+            // действия при получении сообщений
+            public void onReceive(Context context, Intent intent) {
+                String input = intent.getStringExtra(MainActivity.PARAM_LIGTH);
+                tvWind.setText(input);
+
+                meteoState = data[14];
+                switch(meteoState)
+                {
+                    case TRACKING:
+                    {
+                        bServ.setEnabled(true);
+                        bServ.setBackgroundColor(Color.GREEN);
+                        bServ.setText("TRACKING STARTED");
+                        bAlarm.setBackgroundColor(Color.GREEN);
+                        bAlarm.setText("ALARM INACTIVE");
+                    }break;
+
+                    case STOPPED:
+                    {
+                        bServ.setEnabled(true);
+                        bServ.setBackgroundColor(Color.RED);
+                        bServ.setText("TRACKING STOPPED");
+                        bAlarm.setBackgroundColor(Color.GREEN);
+                        bAlarm.setText("ALARM INACTIVE");
+                    }break;
+
+                    case ALARM:
+                    case MANUAL_ALARM:
+                    {
+                        bServ.setEnabled(false);
+                        bAlarm.setText("ALARM ACTIVATED");
+                        bAlarm.setBackgroundColor(Color.RED);
+                    }break;
+                }
+
+
+                tvTime.setText((data[0] & 0xff) + "." + (data[1] & 0xff) + "." + (data[2] & 0xff) + ", " +
+                               (data[3] & 0xff) + ":" + (data[4] & 0xff) + ":" + (data[5] & 0xff));
+                azimuth   = 0.01 * (double)((data[6] & 0xff) | ((data[7] << 8)));
+                elevation = 0.01 * (double)((data[8] & 0xff) | ((data[9] << 8)));
+                tvAzim.setText(String.format("%.1f", azimuth));
+                tvElev.setText(String.format("%.1f", elevation));
+                tvWindS.setText("" + ((data[10] & 0xff) | ((data[11] << 8))));
+                tvlight.setText("" + ((data[12] & 0xff) | ((data[13] << 8))));
+
+                imgPosition.azimuth = azimuth;//sunPos.azimuth;
+//                if(sunPos.elev < 0)
+                    imgPosition.elevation = elevation;
+                //else                imgPosition.elevation = 0;
+                imgSun.invalidate();
+            }
+        };
+        IntentFilter intFilt = new IntentFilter(BROADCAST_ACTION);
+        registerReceiver(br, intFilt);
+
+        //================================================
+        br_cfg = new BroadcastReceiver() {
+            // действия при получении сообщений
+            public void onReceive(Context context, Intent intent) {
+                dialog_set();
+            }
+        };
+
+        IntentFilter intFilt2 = new IntentFilter(BC_CFG_DATA);
+        registerReceiver(br_cfg, intFilt2);
+
+        Button btnSet = (Button) findViewById(R.id.btnMeteoSett);
+        //================================================
+        btnSet.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                byte [] c = new byte[1];
+                c[0] = UDPCommands.GET;
+                UDPCommands.sendCmd(UDPCommands.CMD_CFG, c, ip);
+            }
+        });
+
+        Button wifi = (Button) findViewById(R.id.btnCfgMeteo);
+        //================================================
+        wifi.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dialog_wifi();
+            }
+        });
+
+        Button bSync = (Button) findViewById(R.id.btnMeteoSync);
+        //================================================
+        bSync.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                byte time[] = MainActivity.getCurrentTime();
+                UDPCommands.sendCmd(UDPCommands.CMD_SYNC, time, ip);
+            }
+        });
+
+
+    }
+
+    //==============================================================================================
+    void dialog_set() {
+        final AlertDialog.Builder popDialog = new AlertDialog.Builder(this);
+        final LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View Viewlayout = inflater.inflate(R.layout.dialog_meteo, (ViewGroup) findViewById(R.id.meteo));
+
+        popDialog.setIcon(R.drawable.wifi_small);
+        popDialog.setTitle("Установки");
+        popDialog.setView(Viewlayout);
+
+        final EditText lat = (EditText) Viewlayout.findViewById(R.id.tvMeteoLatit);
+        lat.clearFocus();
+        final EditText lon = (EditText) Viewlayout.findViewById(R.id.tvMeteoLongit);
+        lon.clearFocus();
+        final EditText zone = (EditText) Viewlayout.findViewById(R.id.tvMeteoZone);
+        zone.clearFocus();
+        final EditText wind = (EditText) Viewlayout.findViewById(R.id.tvMeteoWindCFG);
+        wind.clearFocus();
+        final EditText light = (EditText) Viewlayout.findViewById(R.id.tvMeteoLightCFG);
+        light.clearFocus();
+
+
+        double l = 0.01 * ((cfgData[0] & 0xff) | (cfgData[1] << 8));
+        lat.setText  (String.format("%.2f", l).replace(',', '.'));
+        l = 0.01 * ((cfgData[2] & 0xff) | (cfgData[3] << 8));
+        lon.setText  (String.format("%.2f", l).replace(',', '.'));
+
+        zone.setText ("" + ((cfgData[4] & 0xff) | ((cfgData[5] << 8))));
+        wind.setText ("" + ((cfgData[6] & 0xff) | ((cfgData[7] << 8))));
+        light.setText("" + ((cfgData[8] & 0xff) | ((cfgData[9] << 8))));
+
+        popDialog.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+
+                        byte[] tmp = new byte[11];
+                        tmp[0] = UDPCommands.SET;
+                        tmp[1] = (byte) (getInt(lat.getText().toString()) & 0xff);
+                        tmp[2] = (byte) ((getInt(lat.getText().toString()) >> 8) & 0xff);
+                        tmp[3] = (byte) (getInt(lon.getText().toString()) & 0xff);
+                        tmp[4] = (byte) ((getInt(lon.getText().toString()) >> 8) & 0xff);
+                        tmp[5] = (byte) (Integer.parseInt(zone.getText().toString()) & 0xff);
+                        tmp[6] = (byte) ((Integer.parseInt(zone.getText().toString()) >> 8) & 0xff);
+                        tmp[7] = (byte) (Integer.parseInt(wind.getText().toString()) & 0xff);
+                        tmp[8] = (byte) ((Integer.parseInt(wind.getText().toString()) >> 8) & 0xff);
+                        tmp[9] = (byte) (Integer.parseInt(light.getText().toString()) & 0xff);
+                        tmp[10] =(byte) ((Integer.parseInt(light.getText().toString()) >> 8) & 0xff);
+                        UDPCommands.sendCmd(UDPCommands.CMD_CFG, tmp, ip);
+                        dialog.dismiss();
+                    }
+                });
+        popDialog.create();
+        popDialog.show();
+    }
+    //==============================================================================================
+    int  getInt(String str)
+    {
+        return (int)( Double.parseDouble(str) * 100);
+    }
+    //==============================================================================================
+    private String[] wifiMode= {"NULL_MODE","STATION_MODE","SOFTAP_MODE","STATIONAP_MODE"};
+    private String[] wifiSecurityMode = {"AUTH_OPEN","AUTH_WEP","AUTH_WPA_PSK","AUTH_WPA2_PSK","AUTH_WPA_WPA2_PSK","AUTH_MAX"};
+    byte wMode, wSecur;
+    //==============================================================================================
+    void dialog_wifi() {
+        final AlertDialog.Builder popDialog = new AlertDialog.Builder(this);
+        final LayoutInflater inflater = (LayoutInflater) this.getSystemService(LAYOUT_INFLATER_SERVICE);
+        final View Viewlayout = inflater.inflate(R.layout.dialog_wifi, (ViewGroup) findViewById(R.id.wifi));
+
+        popDialog.setIcon(R.drawable.wifi_small);
+        popDialog.setTitle("Установки wifi");
+        popDialog.setView(Viewlayout);
+
+        final EditText ssid = (EditText) Viewlayout.findViewById(R.id.etSSID);
+        ssid.clearFocus();
+        final EditText ssidPass = (EditText) Viewlayout.findViewById(R.id.etSSIDPASS);
+        ssidPass.clearFocus();
+
+        ArrayAdapter<String> adapterW = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, wifiMode);
+        ArrayAdapter<String> adapterS = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, wifiSecurityMode);
+
+        Spinner spinnerW = (Spinner) Viewlayout.findViewById(R.id.spinwWifiMode);
+        Spinner spinnerS = (Spinner) Viewlayout.findViewById(R.id.spinSecur);
+
+        spinnerW.setAdapter(adapterW);
+        spinnerS.setAdapter(adapterS);
+
+        byte[]cfg = loadConfig();
+        if(cfg.length > 0)
+        {
+
+            spinnerS.setSelection((int)cfg[1]);
+            spinnerW.setSelection((int)cfg[0]);
+            String str = new String(cfg);
+            ssid.setText(str.substring(2,str.indexOf('$')));
+            ssidPass.setText(str.substring(str.indexOf('$') + 1, str.length()));
+        }
+
+        spinnerS.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                wSecur = (byte) position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+
+        spinnerW.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                wMode = (byte) position;
+            }
+            @Override
+            public void onNothingSelected(AdapterView<?> arg0) {
+            }
+        });
+
+        popDialog.setPositiveButton("OK",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        String wCfgStr = (char)wMode + "" + (char)wSecur + ssid.getText().toString() + "$" + ssidPass.getText().toString();
+                        saveConfig(wCfgStr);
+                        UDPCommands.sendCmd(UDPCommands.CMD_WIFI, wCfgStr.getBytes(), ip);
+                        dialog.dismiss();
+                    }
+                });
+        popDialog.create();
+        popDialog.show();
+    }
+    //==============================================================================================
+    public static String configReference = "com.voodoo.solar";
+    //==============================================================================================
+    void saveConfig(String aStr) {
+
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(configReference, aStr);
+
+        editor.apply();
+    }
+    //==============================================================================================
+    byte[] loadConfig() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String conf = sharedPreferences.getString(configReference, "") ;
+        return conf.getBytes();
+    }
+    //==============================================================================================
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MainActivity.clientActivityCreated = 0;
+    }
+}
---
+++ Android/SOLAR/app/src/main/java/com/voodoo/solar/sunPos.java
@@ -0,0 +1,174 @@
+package com.voodoo.solar;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.Time;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class sunPos  {
+    public static double DEG_TO_RAD  = 0.01745329;
+    public static double PI =  3.141592654;
+    public static double TWOPI = 6.28318531;
+
+    public static int Hour, Minute = 0, Second = 0, Month = 11, Day = 30, Year, Zone = +2;
+    public static double Lon = -75 * DEG_TO_RAD, Lat = 40 * DEG_TO_RAD;
+
+//    TextView tvResult, tvDate;
+//    EditText etLong, etLatit;
+//
+//    Button btnAnim;
+//
+//    com.voodoo.solar.imgPosition imgSun;
+
+//    private Timer mTimer;
+//    private MyTimerTask mMyTimerTask;
+
+//    @Override
+//    protected void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        setContentView(R.layout.activity_sun_pos);
+//        tvResult = (TextView)findViewById(R.id.tvResult);
+//        tvDate   = (TextView)findViewById(R.id.tvDate);
+//
+//        etLong =  (EditText) findViewById(R.id.etLong);
+//        etLatit =  (EditText) findViewById(R.id.etLatit);
+//
+//        imgSun = (com.voodoo.solar.imgPosition) findViewById(R.id.imgPos);
+//
+////        mTimer = new Timer();
+////        mMyTimerTask = new MyTimerTask();
+////        mTimer.schedule(mMyTimerTask, 1000, 1000);
+//
+//        btnAnim = (Button) findViewById(R.id.btnAnimate);
+//        btnAnim.setOnClickListener(new View.OnClickListener() {
+//            public void onClick(View v) {
+//                if(mTimer == null)
+//                {
+//                    Lon = DEG_TO_RAD * Double.parseDouble(etLong.getText().toString());
+//                    Lat = DEG_TO_RAD * Double.parseDouble(etLatit.getText().toString());
+//                    mTimer = new Timer();
+//                    mMyTimerTask = new MyTimerTask();
+//                    mTimer.schedule(mMyTimerTask, 1000, 1000);
+//                    cntr = 3;
+//                }
+//                else
+//                {
+//                    mTimer.cancel();
+//                    mTimer = null;
+//                    btnAnim.setText("Animate");
+//                }
+//            }
+//        });
+//
+//
+//    }
+//    //==============================================================================================
+//    int cntr = 0;
+    //==============================================================================================
+//    class MyTimerTask extends TimerTask {
+//
+//        @Override
+//        public void run() {
+//            runOnUiThread(new Runnable() {
+//                @Override
+//                public void run() {
+//                    cntr++;
+//                    if(cntr == 22) cntr = 3;
+//                    btnAnim.setText("Sun at " + cntr + " hour");
+//                    Calculate(Year, Month, Day, cntr - Zone, 0, 0);
+//                    imgSun.azimuth   = azimuth / DEG_TO_RAD;
+//                    imgSun.elevation = elev / DEG_TO_RAD;
+//                    imgSun.invalidate();
+//
+//                }
+//            });
+//        }
+//    }
+    //==============================================================================================
+public static void Calculate(int aYear, int aMonth, int aDay, int aHour, int aMinute, int aSecond)
+    {
+        double T, JD_frac, L0, M, e, C, L_true, f, R, GrHrAngle, Obl, RA, Decl, HrAngle;
+        long JD_whole, JDx;
+
+        JD_whole = JulianDate(aYear, aMonth, aDay);
+        JD_frac = (aHour + aMinute/60. + aSecond/3600.)/24. - 0.5;
+        T  = JD_whole - 2451545; T = (T + JD_frac)/36525.;
+        L0 = DEG_TO_RAD * Math.IEEEremainder(280.46645 + 36000.76983 * T, 360);
+        M  = DEG_TO_RAD * Math.IEEEremainder(357.5291  + 35999.0503  * T, 360);
+        e  = 0.016708617 - 0.000042037 * T;
+        C  = DEG_TO_RAD * ((1.9146-0.004847*T)*Math.sin(M) + (0.019993-0.000101*T)*Math.sin(2*M) + 0.00029*Math.sin(3*M));
+        f  = M + C;
+        Obl = DEG_TO_RAD * (23 + 26/60. + 21.448/3600. - 46.815/3600*T);
+        JDx = JD_whole - 2451545;
+        GrHrAngle = 280.46061837 + (360*JDx)%360 + .98564736629*JDx + 360.98564736629*JD_frac;
+        GrHrAngle = Math.IEEEremainder(GrHrAngle,360.);
+        L_true    = Math.IEEEremainder(C+L0,TWOPI);
+        R    = 1.000001018*(1-e*e)/(1+e*Math.cos(f));
+        RA   = Math.atan2(Math.sin(L_true)*Math.cos(Obl),Math.cos(L_true));
+        Decl = Math.asin(Math.sin(Obl)*Math.sin(L_true));
+        HrAngle = DEG_TO_RAD*GrHrAngle+Lon-RA;
+        elev = Math.asin(Math.sin(Lat)*Math.sin(Decl) + Math.cos(Lat)*(Math.cos(Decl)*Math.cos(HrAngle)));
+
+        // Azimuth measured eastward from north.
+        azimuth = PI + Math.atan2(Math.sin(HrAngle),Math.cos(HrAngle)*Math.sin(Lat) - Math.tan(Decl)*Math.cos(Lat));
+    }
+    //==============================================================================================
+    public static double elev, azimuth;
+    //==============================================================================================
+    public static  String getAngles()
+    {
+        String print = "";
+        print += ("Longitude and latitude: ");
+        print += (String.format("%.2f", Lon / DEG_TO_RAD) + ", ");
+        print += (String.format("%.2f", Lat / DEG_TO_RAD) + "\r\n");
+        //print += ("year,month,day,local hour,minute,second,elevation,azimuth");
+        // print+=("\r\n");
+
+        byte tmp[] = MainActivity.getCurrentTime();
+        // Changes may be required in for… loop to get complete
+        // daylight coverage in time zones farther west.
+        for (Hour=0; Hour <= 22; Hour++)
+        {
+            Calculate((tmp[0]+2000), tmp[1],  tmp[2], Hour, 0, 0);
+
+//            print+= year + "." + month + "." + day;  print+=(", ");
+            print+= "| " + sstr(Hour + Zone) + ":" + sstr(Minute) + ":" + sstr(Second);
+
+            print+=(",\te: "); print+= String.format("%.2f", elev    / DEG_TO_RAD);
+            print+=(",\ta: "); print+= String.format("%.2f", azimuth / DEG_TO_RAD);
+            print+=("\r\n");
+
+
+        }
+        return print;
+    }
+    //==============================================================================================
+    public static String sstr(int a)
+    {
+        if(a < 10) return "0" + a;
+        return "" + a;
+    }
+    //==============================================================================================
+    public static long JulianDate(int year, int month, int day) {
+        long JD_whole;
+        int A, B;
+        if (month <= 2) {
+            year--;
+            month += 12;
+        }
+        A = year / 100;
+        B = 2 - A + A / 4;
+        JD_whole = (long) (365.25 * (year + 4716)) + (int) (30.6001 * (month + 1)) + day + B - 1524;
+        return JD_whole;
+    }
+    //==============================================================================================
+}
